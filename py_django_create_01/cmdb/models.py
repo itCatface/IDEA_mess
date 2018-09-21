578f40a17617b5b1c8963c2b526a1ea3584d5581
itCatface <catface.wyh@foxmail.com>
2018-09-21 09:59:37 +0800

    django项目添加sqlite3数据库中数据向页面展示

--- py_django_create_01/cmdb/models.py
+++ py_django_create_01/cmdb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+# 创建model
+class UserInfo(models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
--- py_django_create_01/cmdb/views.py
+++ py_django_create_01/cmdb/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
-
 # Create your views here.
 
 
+user_list = [
+    {'username': 'alistar', 'password': 'ali'},
+    {'username': 'akali', 'password': 'aka'},
+]
+
+
+# def index3(request):
+#     # return HttpResponse("hello world...")
+#
+#     # return render(request, "index.html")
+#
+#     if request.method == 'POST':
+#         username = request.POST.get('username', None)
+#         password = request.POST.get('password', None)
+#         print(username, password)
+#     # return render(request, 'index.html')
+#
+#     # 返回动态页面即返回数据
+#     return render(request, 'index.html', {'data': user_list})
+
+
+from cmdb import models
+
+
 def index(request):
-    # return HttpResponse("hello world...")
-    return render(request, "index.html")
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        models.UserInfo.objects.create(username=username, password=password)
+    user_list = models.UserInfo.objects.all()
+    return render(request, 'index.html', {'data': user_list})
